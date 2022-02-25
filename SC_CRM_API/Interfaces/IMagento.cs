30819515f379aced38dcbbae058cf7669ebe46ff
Gabriel Ceravolo <gabriel.ceravolo+desarollo@sommiercenter.com>
2022-02-25 11:23:46 -0300

    inicial de fusion con integracion magento

---
+++ SC_CRM_API/Interfaces/IMagento.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SC_CRM_API.Interfaces
+{
+    public interface IMagento
+    {
+        //-- Lectura de estatus
+
+    }
+}
